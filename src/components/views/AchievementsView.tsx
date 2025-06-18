7f6a597771e1b90bbc0d4998c95b0d154b0e94d7
ayatchenko <antony3595@gmail.com>
2025-06-18 23:10:28 +0600

    + update AchievementsView.tsx stylesheets and add count badge

--- src/components/views/AchievementsView.tsx
+++ src/components/views/AchievementsView.tsx
@@ -2,6 +2,7 @@ import { useAchievementsLoader } from "@/redux/game/loader.ts";
 import Page from "@/components/views/Page.tsx";
 import strings from "@/constants/strings.ts";
 import Typer from "@/components/ui/Typer/typer.tsx";
+import { Badge } from "@/components/ui/badge.tsx";
 
 const AchievementsView = () => {
 	const [isAchievementsLoading, achievements] = useAchievementsLoader();
@@ -9,15 +10,25 @@ const AchievementsView = () => {
 		<Page centrify={isAchievementsLoading}>
 			<div className="min-h-full flex">
 				<div className={"w-full flex flex-col"}>
-					<div className="flex gap-2 grid-cols-2">
-						{achievements.map((achievement) => {
-							return (
-								<div key={achievement.id} className={"border rounded-sm p-2 "}>
+					<div className={"flex flex-col gap-2"}>
+						{achievements.map((achievement) => (
+							<div className={"border rounded-sm  flex flex-row relative"}>
+								<Badge
+									key={achievement.id}
+									className="h-5 min-w-5 rounded-full px-1 absolute"
+									style={{
+										right: -6,
+										top: -6,
+									}}
+								>
+									{achievement.count}
+								</Badge>
+								<div className={"overflow-hidden rounded-sm border-sm w-40 min-w-40"}>
 									<img src={achievement.image_url} alt="" />
-									<div className={"mt-2 text-xs"}>{achievement.title}</div>
 								</div>
-							);
-						})}
+								<div className={"mt-2 p-2 flex-grow-1 text-sm"}>{achievement.title}</div>
+							</div>
+						))}
 					</div>
 					{!isAchievementsLoading && !achievements.length && (
 						<div className="my-auto w-full">
