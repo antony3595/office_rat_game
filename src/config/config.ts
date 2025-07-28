fe697e6d08f6718e438044032780ad2b1503c76a
ayatchenko <antony3595@gmail.com>
2025-07-29 00:01:26 +0600

    + update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,7 +21,7 @@ const buildTypeConfigs: BuildTypesScheme = {
 		API_URL: "http://127.0.0.1:8000/",
 	},
 	[BuildTypeEnum.PRODUCTION]: {
-		API_URL: "https://unknown.kg/",
+		API_URL: "https://office-rat-game.kyrgyzstan.kg/api/",
 		BUILD_TYPE: BuildTypeEnum.PRODUCTION,
 	},
 	[BuildTypeEnum.MOCK]: {
